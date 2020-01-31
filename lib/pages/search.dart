5cc9311cacb197c0797261fe0c0fdaec355db9d6
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-01-31 16:41:05 +0100

    Addes the search page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/timeline.dart';
+import 'package:fluttershare/widgets/progress.dart';
+import 'package:timeago/timeago.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,22 +13,61 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResultsFuture;
+
+  clearSearch() {
+    searchController.clear();
+  }
+
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = usersRef
+        .where('displayName', isGreaterThanOrEqualTo: query)
+        .getDocuments();
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+
+  buildSearchResults() {
+    return FutureBuilder(
+      future: searchResultsFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        List<UserResult> searchResults = [];
+        snapshot.data.documents.forEach((doc) {
+          User user = User.fromDocument(doc);
+          UserResult searchResult = UserResult(user);
+          searchResults.add(searchResult);
+        });
+        return ListView(
+          children: searchResults,
+        );
+      },
+    );
+  }
+
   AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         autocorrect: true,
         decoration: InputDecoration(
-            hintText: "Search for a user",
-            filled: true,
-            prefixIcon: Icon(
-              Icons.account_box,
-              size: 28.0,
-            ),
-            suffixIcon: IconButton(
-              icon: Icon(Icons.clear),
-              onPressed: () => print("cleared"),
-            )),
+          hintText: "Search for a user",
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28.0,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => clearSearch,
+          ),
+        ),
+        onFieldSubmitted: handleSearch,
       ),
     );
   }
@@ -56,14 +101,41 @@ class _SearchState extends State<Search> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildSearchField(),
-      body: buildNoContent(),
+      body:
+          searchResultsFuture == null ? buildNoContent() : buildSearchResults(),
     );
   }
 }
 
 class UserResult extends StatelessWidget {
+  final User user;
+
+  UserResult(this.user);
   @override
   Widget build(BuildContext context) {
-    return Text("User Result");
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.3),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+              ),
+              title: Text(
+                user.displayName,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                user.username,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
